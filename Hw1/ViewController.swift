6778a9d1999c9ece4f7993392386662e4e69193e
Aidar Nurkin <aidar.nurkin@icloud.com>
2023-01-17 17:47:34 +0600

    Traffic light with some bad uikit front

--- Hw1/ViewController.swift
+++ Hw1/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var YellowLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    
+    var changedon = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        YellowLabel.alpha = 0.5
+        greenLabel.alpha = 0.5
     }
 
-
+    @IBAction func changeColorsButton() {
+        switch changedon {
+        case 0:
+            redLabel.alpha = 1
+            greenLabel.alpha = 0.5
+            changedon = 1
+        case 1:
+            redLabel.alpha = 0.5
+            YellowLabel.alpha = 1
+            changedon = 2
+        case 2:
+            YellowLabel.alpha = 0.5
+            greenLabel.alpha = 1
+            changedon = 0
+        default:
+            break
+        }
+    }
+    
 }
 
